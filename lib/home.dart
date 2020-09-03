1aff302693abcf1234d38739f48c5910ac2eae12
Luis Barbon <luisbarbon@gmail.com>
2020-09-03 06:08:40 -0300

    atualizacao-plugins
    Plugin Webview atualizado, removido controller que nao estava sendo usado

--- lib/home.dart
+++ lib/home.dart
@@ -5,7 +5,6 @@ import 'package:webview_flutter/webview_flutter.dart';
 void main() => runApp(Home());
 
 class Home extends StatelessWidget {
-  WebViewController _controller;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -13,9 +12,7 @@ class Home extends StatelessWidget {
         body: WebView(
           initialUrl: 'http://sislimpa.pedido.la',
           javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController c) {
-            _controller = c;
-          }),
+        ),
       ),
     );
   }
